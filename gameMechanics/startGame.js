f6fb0482a0ffc88294756e378b124b01eeae754f
w1tness <creativesxmaestro@gmail.com>
2018-11-07 19:38:32 +0800

    Further improvements on scoring

--- gameMechanics/startGame.js
+++ gameMechanics/startGame.js
@@ -1,4 +1,5 @@
 var counter = 60000;
+var countTime = false;
 AFRAME.registerComponent('starting',{
             init: function(){
                 let sceneEl = document.querySelector('a-scene'); 
@@ -14,10 +15,9 @@ AFRAME.registerComponent('starting',{
             tick: function(){
                 let sceneEl = document.querySelector('a-scene');
                 let text = document.querySelector('#txt');
-                counterString = counter.toString();
-                counter = counter - 1;
-                text.setAttribute('text-geometry', 'value: ' + counterString);
-                console.log(counterString);
+     //           counterString = counter.toString();
+      //          counter = counter - 1;
+                //text.setAttribute('text-geometry', 'value: ' + counterString);
             }
 
 }); 
@@ -29,3 +29,38 @@ AFRAME.registerComponent('umUmUmUm', {
           })
         }
 })
+
+AFRAME.registerComponent('click-listener', {
+  init: function () {
+    var el = this.el;
+    window.addEventListener('click', function () {
+    countTime = true; 
+    });
+  },
+    tick: function(){
+    if(countTime){
+        let sceneEl = document.querySelector('a-scene');
+        let text = document.querySelector('#txt');
+        counterString = counter.toString();
+        counter = counter - 1;
+        console.log(counter);
+        text.setAttribute('text-geometry', 'value: ' + counterString);
+    } 
+    }
+});
+
+AFRAME.registerComponent('score-track', {
+    schema: {
+        on: { default: 'click' },
+    },
+    update: function (oldData) {
+        this.el.addEventListener(this.data.on, this.txck.bind(this));
+    },
+    txck: function () {
+        console.log('hii'); 
+    }
+//    el.sceneEl.appendChild(entity);
+});
+
+
+
