20912a3315b7e28b44c860d7cf98126153191586
w1tness <creativesxmaestro@gmail.com>
2018-11-07 19:00:54 +0800

    Scoring Fundamentals Created

--- gameMechanics/startGame.js
+++ gameMechanics/startGame.js
@@ -1,3 +1,4 @@
+var counter = 60000;
 AFRAME.registerComponent('starting',{
             init: function(){
                 let sceneEl = document.querySelector('a-scene'); 
@@ -9,7 +10,16 @@ AFRAME.registerComponent('starting',{
                     let visGroup = document.querySelector('#modelWrap');
                     visGroup.setAttribute('visible', 'true');
                 }, 9000);
-                }
+                },
+            tick: function(){
+                let sceneEl = document.querySelector('a-scene');
+                let text = document.querySelector('#txt');
+                counterString = counter.toString();
+                counter = counter - 1;
+                text.setAttribute('text-geometry', 'value: ' + counterString);
+                console.log(counterString);
+            }
+
 }); 
 
 AFRAME.registerComponent('umUmUmUm', {
